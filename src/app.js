ede69050c8ba2c1b5f1f6f0f25f912957b697902
Nadinvs <nadinvs@gmail.com>
2022-10-22 16:13:19 +0300

    wind

--- src/app.js
+++ src/app.js
@@ -1,1 +1,18 @@
-//alert("Wold");
+function displayTemperature(response) {
+  console.log(response.data);
+  let temperatureElement = document.querySelector("#temperature");
+  let cityElement = document.querySelector("#city");
+  let descriptionElement = document.querySelector("#description");
+  let humidityElement = document.querySelector("#humidity");
+  let windElement = document.querySelector("#wind");
+  temperatureElement.innerHTML = Math.round(response.data.main.temp);
+  cityElement.innerHTML = response.data.name;
+  descriptionElement.innerHTML = response.data.weather[0].description;
+  humidityElement.innerHTML = response.data.main.humidity;
+  windElement.innerHTML = Math.round(response.data.wind.speed);
+}
+
+let apiKey = "a99a05f4d11bbb239e323bbb1d7f5d4e";
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=Paris&appid=${apiKey}&units=metric`;
+
+axios.get(apiUrl).then(displayTemperature);
